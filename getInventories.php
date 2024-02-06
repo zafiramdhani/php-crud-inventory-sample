1e8e699c417667c58ac883b6764cfa502ea065c6
zafiramdhani <izzafiramdhanialkaff@gmail.com>
2024-02-06 13:03:59 +0700

    Add order by latest

--- getInventories.php
+++ getInventories.php
@@ -10,7 +10,7 @@ $query = mysqli_query($conn, "SELECT COUNT(*) FROM inventories");
 $row = mysqli_fetch_row($query);
 $result['total'] = $row[0];
 
-$query = mysqli_query($conn, "SELECT * FROM inventories LIMIT $offset, $rows");
+$query = mysqli_query($conn, "SELECT * FROM inventories ORDER BY created_at DESC LIMIT $offset, $rows");
 
 $items = array();
 while ($row = mysqli_fetch_object($query)) {
